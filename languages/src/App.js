d18abb6e2335d63fbac52e3d1a8b923b4be1c117
tianasama <tianaattride@gmail.coM>
2019-04-22 22:14:50 -0400

    Updated styling

--- languages/src/App.js
+++ languages/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 
 // Bootstrap
 import Container from 'react-bootstrap/Container';
@@ -7,12 +8,16 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 
+// React Router
+
+
 // Components
 import About from "./components/About";
 import Hour from "./components/Hour";
 import Week from "./components/Week";
 import Map from "./components/Map";
 import Table from "./components/Table";
+import Pick from "./components/Pick";
 import Resources from "./components/Resources";
 
 // Stylesheets
@@ -22,10 +27,52 @@ import './styles.css';
    * Renders home page.
    * @return {any} JSX content
    */
+function Index() {
+  return (
+    <div id="mainBody">
+      <div id="main">
+        <Container>
+          <Row>
+            <Col>
+              <h1 id="title">Did you know that only one in five Americans<br />speak a second language?</h1>
+            </Col>
+          </Row>
+        </Container>
+        <Container>
+          <Row>
+            <Col>
+              <img
+                id="earth-title"
+                alt="earth"
+                src={require('./media/planet.png')}
+                width="400"
+                height="400"
+                className="center-block"
+              />
+            </Col>
+          </Row>
+          <Row id='title-text'>
+            <Col><br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu lectus ac purus lacinia consequat et ac nulla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec elit libero, elementum ut mi eget, tristique malesuada lorem. Proin semper in ante at mollis. Quisque eget sapien massa. Praesent ac sem vel libero tempor pellentesque. In non congue nulla. In non congue nulla. In non congue nulla.</Col>
+            <Col><br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu lectus ac purus lacinia consequat et ac nulla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec elit libero, elementum ut mi eget, tristique malesuada lorem. Proin semper in ante at mollis. Quisque eget sapien massa. Praesent ac sem vel libero tempor pellentesque. In non congue nulla. In non congue nulla. In non congue nulla.</Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+}
+
+/**
+   * Creates app component.
+   * @return {any} Tooltip JSX
+   */
 class App extends Component {
+  /**
+     * Renders app.
+     * @return {any} JSX content
+     */
   render() {
     return (
-      <div id="mainBody">
+      <div>
         <div id="#myNavbar">
           <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky="top">
             <Navbar.Brand href="#navTitle">
@@ -41,10 +88,10 @@ class App extends Component {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link href="#">home</Nav.Link>
-                <Nav.Link href="/About">about</Nav.Link>
-                <Nav.Link href="/Language">picking a language</Nav.Link>
-                <Nav.Link href="/Resource">resources</Nav.Link>
+                <Nav.Link href="/">home</Nav.Link>
+                <Nav.Link href="#/about">about </Nav.Link>
+                <Nav.Link href="#/pick">pick a language</Nav.Link>
+                <Nav.Link href="#/resources/">resources</Nav.Link>
               </Nav>
               <Nav>
                 <Nav.Link href="https://www.state.gov/documents/organization/247092.pdf" target="_blank" rel="noopener noreferrer">data</Nav.Link>
@@ -55,35 +102,15 @@ class App extends Component {
             </Navbar.Collapse>
           </Navbar>
         </div>
-        <div id="main">
-          <Container>
-            <Row>
-              <Col>
-                <h1 id="title">Did you know that only one in five Americans<br />speak a second language?</h1>
-              </Col>
-            </Row>
-          </Container>
-          <Container>
-            <Row>
-              <Col>
-                <img
-                  id="earth-title"
-                  alt="earth"
-                  src={require('./media/planet.png')}
-                  width="400"
-                  height="400"
-                  className="center-block"
-                />
-              </Col>
-            </Row>
-            <Row id='title-text'>
-              <Col><br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu lectus ac purus lacinia consequat et ac nulla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec elit libero, elementum ut mi eget, tristique malesuada lorem. Proin semper in ante at mollis. Quisque eget sapien massa. Praesent ac sem vel libero tempor pellentesque. In non congue nulla. In non congue nulla. In non congue nulla.</Col>
-              <Col><br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu lectus ac purus lacinia consequat et ac nulla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec elit libero, elementum ut mi eget, tristique malesuada lorem. Proin semper in ante at mollis. Quisque eget sapien massa. Praesent ac sem vel libero tempor pellentesque. In non congue nulla. In non congue nulla. In non congue nulla.</Col>
-            </Row>
-          </Container>
-        </div>
+        <Router>
+          <div>
+            <Route path="/" exact component={Index} />
+            <Route path="/about/" component={About} />
+            <Route path="/pick/" component={Pick} />
+            <Route path="/resources/" component={Resources} />
+          </div>
+        </Router>
       </div>
-
 
     );
   }
--- languages/src/components/About/index.js
+++ languages/src/components/About/index.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 // Bootstrap
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
+import Tab from 'react-bootstrap/Tab';
 
 // Stylesheets
 import "./styles.css";
@@ -16,7 +18,105 @@ import "./styles.css";
 class About extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <h1 id="title">About</h1>
+        <Container>
+          <Row>
+            <Col id="intro">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla nec enim vel fringilla. Vestibulum cursus fermentum ante a posuere. Mauris eget ligula non magna convallis varius. In dapibus gravida ex. Phasellus iaculis sem ullamcorper purus finibus, ac porta mi pretium. Maecenas ac tellus imperdiet, fermentum ipsum id, consequat libero. Vivamus sed ex id urna iaculis iaculis et ut ligula. Etiam tempus metus ut urna euismod, nec sodales sem sagittis. Vestibulum ac mi interdum, eleifend mauris ut, dignissim dolor.
+            </Col>
+          </Row>
+        </Container>
+
+        {/* Tabs */}
+        <div id="category-tabs">
+          <Tab.Container defaultActiveKey="first">
+            <Row>
+              <Col sm={3}>
+                <Nav variant="pills" className="flex-column">
+                  <Nav.Item>
+                    <Nav.Link eventKey="first">World</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="second">Moderate</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="third">Hard</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="fourth">Advanced</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Col>
+              <Col sm={9}>
+                <Tab.Content>
+                  <Tab.Pane eventKey="first">
+                    <h1 className="section-header">World languages</h1>
+                    <Row>
+                      <Col>World languages denotes those languages that are easiest for English speakers to learn. Nullam fringilla nec enim vel fringilla. Vestibulum cursus fermentum ante a posuere. Mauris eget ligula non magna convallis varius.</Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <h3 className="small-header">Such languages include:</h3>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col></Col>
+                      <Col></Col>
+                    </Row>
+                  </Tab.Pane>
+
+                  <Tab.Pane eventKey="second">
+                    <h1 className="section-header">Moderate languages</h1>
+                    <Row>
+                      <Col>Moderate languages denotes those languages that are moderately difficult for English speakers to learn. Nullam fringilla nec enim vel fringilla. Vestibulum cursus fermentum ante a posuere. Mauris eget ligula non magna convallis varius.</Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <h3 className="small-header">Such languages include:</h3>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col></Col>
+                      <Col></Col>
+                    </Row>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="third">
+                    <h1 className="section-header">Hard languages</h1>
+                    <Row>
+                      <Col>Hard languages denotes those languages that are difficult for English speakers to learn. Nullam fringilla nec enim vel fringilla. Vestibulum cursus fermentum ante a posuere. Mauris eget ligula non magna convallis varius.</Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <h3 className="small-header">Such languages include:</h3>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col></Col>
+                      <Col></Col>
+                    </Row>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="fourth">
+                    <h1 className="section-header">Advanced languages</h1>
+                    <Row>
+                      <Col>Advanced languages denotes those languages that are extremely difficult for English speakers to learn. Nullam fringilla nec enim vel fringilla. Vestibulum cursus fermentum ante a posuere. Mauris eget ligula non magna convallis varius.</Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <h3 className="small-header">Such languages include:</h3>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col></Col>
+                      <Col></Col>
+                    </Row>
+                  </Tab.Pane>
+                </Tab.Content>
+              </Col>
+            </Row>
+          </Tab.Container>
+        </div>
+      </div>
     );
   }
 }
