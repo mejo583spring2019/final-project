d0bd96c9c1a3842a52420175343a79fd8ac9dd6a
“SivonneWard” <sivonne@live.unc.edu>
2019-04-23 21:39:11 -0400

    editing data-tree.js, following week 12 hw 1 videos for data setup and printing person + character names to the console

--- simpsons-data/utils/data-tree.js
+++ simpsons-data/utils/data-tree.js
@@ -4,19 +4,39 @@ const fetch = require("node-fetch");
 
 const dataURL = "http://api.tvmaze.com/shows/83/cast"
     ;
-const numberOfCharactersPlayed = {};
+const personName = {};
+//
 
-
+/** this is a JSDOC comment
+*/
 function makeTree(fullData) {
   fullData.forEach((r) => {
-    console.log("the current record is", r);
-    byYear[r.year] = r;
-  });
-}
-return numberOfCharactersPlayed;
+    const strPerson = String(r.person.name);
+    const strCharacter = String(r.character.name);
 
+    let personData = personName[r.person.name];
+    if (personData === undefined) {
+      personData = {};
+    }
+    let makeData = personData[r.character.name];
+    if (makeData === undefined) {
+      makeData = {};
+    }
+
+    makeData[r.character.image.medium] = r.character.url; // should do r.id? from video
+    personData[r.character.name] = makeData;
+
+    personName[strPerson] = personData;
+  });
+
+  console.log(personName["Julie Kavner"]);
+
+  //   console.log(personName);
+  return personName;
+}
+/** this is a JSDOC comment */
 function main() {
-  console.log(dataURL);
+  //   console.log(dataURL);
   fetch(dataURL)
       .then((res) => res.json())
       .then((json) => makeTree(json));
