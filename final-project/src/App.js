91d73d800ecc2d569f5e91326b847beba5214c2f
torin <torin@live.unc.edu>
2019-04-29 14:26:24 -0400

    linting

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Jumbotrons from "./components/layout/jumbotron";
 import Nav from "./components/layout/navigation";
 import Cards from "./components/elements/card";
-import Youtube from './components/elements/youtube';
 
 import "./app.css";
 
@@ -25,18 +24,8 @@ class App extends Component {
         <Nav />
         <Jumbotrons />
         <div className="outer">
-        <Youtube />
         </div>
-        <div className="outer">
-        <div className="inner center width">
-
-        <p className="j-text">
-              During the Great Depression, food banks, homeless shelters and local government offices were flooded by needy men, women and families who were out of money and in need of critical aid. As a result, President Franklin D. Roosevelt made it his mission to get The New Deal enacted to create jobs and provide citizens with a way out of poverty. This created social support programs such as Medicare, Medicaid, Social Security and more. <br /><br />
-              Unfortunately, after almost 90 years, these programs have been unable to keep up with the rising needs of citizens and the wage gap between the wealthy and the less fortunate has steadily grown. As a result, many economists have begun searching for a new solution to the issue of poverty. One option that is gaining traction is Universal Basic Income. 
-            </p>
-          </div>
-
-          </div>
+      
         <div className="outer">
        
           <Cards />
--- final-project/src/components/elements/youtube.js
+++ final-project/src/components/elements/youtube.js
@@ -1,37 +1,40 @@
-import React, { Component } from 'react';
-import YouTube from 'react-youtube';
+import React, { Component } from "react";
+import YouTube from "react-youtube";
 
-
-
+/** creates a new class, Youtube, that makes a Youtube iframe on page
+ * @return {any}
+ */
 class Youtube extends Component {
+  /** when the window is ready accesses player
+   * @event Youtube#_onReady
+   * @type {object}
+   */
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+  /** renders the iframe
+   * @return {Object}
+  */
+  render() {
+    const opts = {
+      height: "500",
+      width: "877",
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
 
-    _onReady(event) {
-        // access to player in all event handlers via event.target
-        event.target.pauseVideo();
-      }
-    render() {
-        const opts = {
-          height: '500',
-          width: '877',
-          playerVars: { // https://developers.google.com/youtube/player_parameters
-            autoplay: 1
-          }
-        };
-    
-        return (
-          <YouTube
-            videoId="kl39KHS07Xc"
-            opts={opts}
-            onReady={this._onReady}
-          />
-        );
-      }
-    
-     
-    }
+    return (
+      <YouTube
+        videoId="kl39KHS07Xc"
+        opts={opts}
+        onReady={this._onReady}
+      />
+    );
+  }
+}
 
 export default Youtube;
 
 
-
-// youtube id: , for UBI video
