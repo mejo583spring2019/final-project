d1d68f30867476b31d943836bae947ff0e2a0ee9
Bridget Williams <bridget_williams1@unc.edu>
2019-04-30 01:06:05 -0400

    Updated map and chart

--- air-quality-data/src/App.js
+++ air-quality-data/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Map from "././components/Map"
+import LineChart from './components/LineChart';
+import "./index.css";
+
 
 function App() {
   return (
-    <div>Bridget's Final Project
+    <div id="main-container">
+      <menu></menu>
+      <h1>Air Quality and Car Ownership</h1>
       <Map />
+      <LineChart />
     </div>
   );
 }
