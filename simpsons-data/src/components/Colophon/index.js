a976432996bf1aca53734228cd5c9c1a286cc2e0
“SivonneWard” <sivonne@live.unc.edu>
2019-04-29 21:57:00 -0400

    added more to the colophon, going to deploy

--- simpsons-data/src/components/Colophon/index.js
+++ simpsons-data/src/components/Colophon/index.js
@@ -21,15 +21,17 @@ class Colophon extends React.Component {
             <Scene pin>
               <div className="panel colophon">
                 <h2>Colophon</h2>
-                <h4>Why did I make this site?</h4>
+                <h3>Why did I make this site?</h3>
                 <p>As a senior multimedia major, I created this final project
                 as a demonstration of some of the skills we have been
                 learning in our advanced multimedia class.
                  This project demonstrates some understanding
                 of the basics of data visualization,
-                using the tool High Charts, as well as
-                 some structuring tools such as Scroll
-                  Magic and React as a framework.
+                using the tool <a href="https://www.highcharts.com/docs/chart-and-series-types/packed-bubble-charts
+">High Charts,</a> as well as
+                 some structuring tools such as <a href="https://bitworking.github.io/react-scrollmagic/section-wipes
+">Scroll Magic</a> and <a href="https://reactjs.org/">React</a> to format everything. To lint my code, I used <a href="https://eslint.org/">ESLint</a>
+ and <a href="https://github.com/google/eslint-config-google">Eslint Config Google</a>. To check my code was linted correctly to fit the class standards, I ran it through <a href="https://circleci.com/">Circle CI</a>
                 <br/><br/>
                 I found all of my Simpsons data from TVMaze.com,
                      from their API, which gave me a data file
@@ -48,7 +50,6 @@ class Colophon extends React.Component {
                               API file I used
                               earlier).
                 <br/><br/>
-                {/* <h4>How did you get the data into the app?</h4> */}
                 To get the data into the application, I did a series of
                      programmatic data cleanup, pulling from the url into
                       a data file, which I then manually cleaned up and
@@ -58,9 +59,6 @@ class Colophon extends React.Component {
                           High Charts data (because it wasn't included
                            in the API).
                 <br/><br/>
-                {/* <h4>What tools / libraries /
-                frameworks are you using?</h4>*/}
-
                 To structure the assignment, I used the extremely helpful
                 <a href="https://reactjs.org/">React</a> library.
                 Alongside the React base, I used various components,
@@ -88,8 +86,12 @@ class Colophon extends React.Component {
                    looking at some Simpsons photos for reference.
                    The photos that I used for the cast
                    members were also retrieved from TVMaze.com's
-                   API<a href="http://api.tvmaze.com/shows/83/cast">API</a>
-                   Additionally, I used font-awesome for an <a href="https://fontawesome.com/icons/angle-down?style=solid">svg arrow</a> for scrolling.
+                <a href="http://api.tvmaze.com/shows/83/cast"> API</a>.
+                   Additionally, I used font-awesome for an <a
+                  href="https://fontawesome.com/icons/angle-down?style=solid">svg arrow </a> for scrolling.
+                <br/>
+                <br/>
+                   This app was deployed using <a href="https://github.com/sivonne/simpsons-app">Github Pages.</a>
                 </p>
               </div>
             </Scene>
